ccd660ddec585809ddea696190a0d27641df5abc
Sam Meester <sammeester1@gmail.com>
2022-09-25 10:45:05 -0700

    Initial Commit

---
+++ NebulaBar/NebulaBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebulaBarApp.swift
+//  NebulaBar
+//
+//  Created by Sam Meester on 2022-09-25.
+//
+
+import SwiftUI
+
+@main
+struct NebulaBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
